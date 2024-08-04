79e13983e1f555d67ba7b3873ec983d2a1d26862
Avriaryn <avriarynn@proton.me>
2024-08-04 14:59:00 +0930

    Updated to handle invalid moves and display chosen moves

--- Referee.cpp
+++ Referee.cpp
@@ -1,11 +1,22 @@
 #include "Referee.h"
+#include <iostream>
 
+// Defualt Constructor for referee class
 Referee::Referee() {}
 
 // refGame function
 Player* Referee::refGame(Player* player1, Player* player2) {
     Move* move1 = player1->makeMove();
     Move* move2 = player2->makeMove();
+
+    // Input validation
+    if (move1 == nullptr || move2 == nullptr) {
+        std::cerr << "Invalid move. Exiting game." << std::endl;
+        return nullptr;
+    }
+
+    std::cout << player1->getName() << " chose " << move1->getName() << std::endl;
+    std::cout << player2->getName() << " chose " << move2->getName() << std::endl;
 
     if (move1->getName() == move2->getName()) {
         return nullptr; // tie
