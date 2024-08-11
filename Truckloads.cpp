840fceb8c08675d100cef5c3b46c7ae99e3a4b63
Avriaryn <avriarynn@proton.me>
2024-08-11 14:02:12 +0930

    Updated Truckloads.cpp

--- Truckloads.cpp
+++ Truckloads.cpp
@@ -1,25 +1,16 @@
 #include <iostream>
+#include "Truckloads.h"  
 
-class Truckloads {
-public:
-    int numTrucks(int numCrates, int loadSize) {
-        if (numCrates <= loadSize) {
-            return 1;
-        }
-        
-        // Recursive case which divides the crates into two smaller piles
-        int half1 = numCrates / 2;
-        int half2 = numCrates - half1;
-        
-        // Calcs the sum of the trucks required for both piles
-        return numTrucks(half1, loadSize) + numTrucks(half2, loadSize);
+// numTrucks method
+int Truckloads::numTrucks(int numCrates, int loadSize) {
+    if (numCrates <= loadSize) {
+        return 1;
     }
-};
-
-int main() {
-    Truckloads tl;
-    std::cout << tl.numTrucks(14, 3) << std::endl; // Must return 6
-    std::cout << tl.numTrucks(15, 1) << std::endl; // Must return 15
-    std::cout << tl.numTrucks(1024, 5) << std::endl; // Must return 256
-    return 0;
+    
+    // Recursive case which divides the crates into two smaller piles
+    int half1 = numCrates / 2;
+    int half2 = numCrates - half1;
+    
+    // Calcs the sum of the trucks required for both piles
+    return numTrucks(half1, loadSize) + numTrucks(half2, loadSize);
 }
