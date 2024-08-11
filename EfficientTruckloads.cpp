39f04c545d2e9a178cbfbe1d7272a54b1342a6ce
Avriaryn <avriarynn@proton.me>
2024-08-11 21:40:33 +0930

    Fixed EfficientTruckloads.cpp

--- EfficientTruckloads.cpp
+++ EfficientTruckloads.cpp
@@ -1,24 +1,21 @@
 #include "EfficientTruckloads.h"
 
-// Constructor
 EfficientTruckloads::EfficientTruckloads() {
-    // Initialization if needed
 }
 
-// Implementation of the numTrucks method with memoization
+// Attempt of the numTrucks method with memoisation
 int EfficientTruckloads::numTrucks(int numCrates, int loadSize) {
     // Check if the result is already in the memo
     if (memo.find(numCrates) != memo.end() && memo[numCrates].find(loadSize) != memo[numCrates].end()) {
         return memo[numCrates][loadSize];
     }
 
-    // Base case: If the number of crates is less than or equal to loadSize,
-    // then it can fit on one truck.
+    // If the number of crates is less than or equal to loadSize, then it can fit on one truck
     if (numCrates <= loadSize) {
         return memo[numCrates][loadSize] = 1;
     }
 
-    // Recursive case: divide the crates into two smaller piles.
+    // Recursive function which divide the crates into two smaller piles
     int half1 = numCrates / 2;
     int half2 = numCrates - half1;
 
