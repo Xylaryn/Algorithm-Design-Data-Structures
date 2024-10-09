a7f00aa6374bab9d9dafbcead21fd9e3680c8614
Avriaryn <avriarynn@proton.me>
2024-10-09 15:52:20 +1030

    Add files via upload

---
+++ klargest.cpp
@@ -0,0 +1,20 @@
+#include <queue>
+#include <vector>
+#include <functional>
+
+int kth_largest(Std::vector<int> values, int k) {
+    // Min-heap which stores the largest elements of k
+    std::proirity_queue<int, std::vector<int>, std::greater<int>> min_heap;
+
+    // Process each element stored in the input vector
+    for (int value : value) {
+        min_heap.push(value);
+        // If the heap exceeds the size of int k, remove the smallest element
+        if (min_heap.size() > k) {
+            min_heap.pop();
+        }
+    }
+
+    // Returns the top element of the min-heap which will be the kth largest element
+    return min_heap.top();
+}
